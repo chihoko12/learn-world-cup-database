ee73ff404a8231c901c8a462717c5db3e7a4960b
Chihoko Neudeck <chihoko.ueno@gmail.com>
2024-03-24 20:12:06 +0000

    complete insert_data.sh

--- queries.sh
+++ queries.sh
@@ -3,7 +3,7 @@
 PSQL="psql --username=freecodecamp --dbname=worldcup --no-align --tuples-only -c"
 
 # Do not change code above this line. Use the PSQL variable above to query your database.
-
+# pg_dump -cC --inserts -U freecodecamp worldcup > worldcup.sql psql -U postgres < worldcup.sql
 echo -e "\nTotal number of goals in all games from winning teams:"
 echo "$($PSQL "SELECT SUM(winner_goals) FROM games")"
 
